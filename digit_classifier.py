d7bc1b2f94f826e3d47212e87f5737e4fc81b2db
Minyoung-Lee <leemy08@naver.com>
2022-01-25 23:44:09 +0900

    tensorflow import

---
+++ digit_classifier.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+print("tensorflow version: ", tf.__version__)
